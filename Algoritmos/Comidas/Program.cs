4de0789e4facbde629ecf32ba56eb2cc946dab42
dylan <dylanrios211@gmail.com>
2025-04-09 09:16:50 -0500

    solucion de error del codigo

--- Algoritmos/Comidas/Program.cs
+++ Algoritmos/Comidas/Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool continuar = true;
+int j;
+List<string> seleccion = new List<string>();
+string[] desayuno = {"Cafe con Leche","Cafe negro","Calentado","Empanada","Pandebono",
+    "Hamburguesa","Cuca Rosada" };
+string[] almuerzo = {"Arroz","Pastas","Calentado","Bandeja paisa","Carne a la llanera",
+    "Lechona","Sopa","Mondogo","Chorizo con chimichurry","Lazaña","Hamburguesas","Cazuela de mariscos","Tacos",
+    "Costillas","Jugo","Gaseosa","Limonada"};
+string[] cena = {"Hamburguesa","Pizaa","Sushi","Pastas","Picada","PanChocha","Salchipapas","Punta de anca",
+    "Michelada","Gaseosa"};
+while(continuar)
+{
+    Console.WriteLine("Eliga la opcion que desee...");
+    Console.WriteLine("1.DESAYUNO");
+    Console.WriteLine("2.ALMUERZO");
+    Console.WriteLine("3.CENA");
+    Console.WriteLine("4.SALIR");
+    string opcionComida = Console.ReadLine();
+    int opcion;
+    if (!int.TryParse(opcionComida, out opcion))
+    {
+        Console.WriteLine("Por favor, ingrese un número válido.");
+        continue;
+    }
+    switch (opcion)
+    {
+        case 1:
+            for (j = 0; j < desayuno.Length; j++)
+            {
+                Console.WriteLine($"¿Quieres {desayuno[j]}? (si/no): ");
+                string respuesta = Console.ReadLine();
+                if (respuesta == "si")
+                {
+                    seleccion.Add(desayuno[j]);
+                }
+            }
+            break;
+        case 2:
+            for (j = 0; j < almuerzo.Length; j++)
+            {
+                Console.WriteLine($"¿Quieres {almuerzo[j]}? (si/no): ");
+                string respuesta = Console.ReadLine();
+                if (respuesta == "si")
+                {
+                    seleccion.Add(almuerzo[j]);
+                }
+            }
+            break;
+        case 3:
+            for (j = 0; j < cena.Length; j++)
+            {
+                Console.WriteLine($"¿Quieres {cena[j]}? (si/no): ");
+                string respuesta = Console.ReadLine();
+                if (respuesta == "si")
+                {
+                    seleccion.Add(cena[j]);
+                }
+            }
+            break;
+        case 4:
+            continuar = false;
+            break;
+    }
+    Console.WriteLine("Los productos que elegiste son: ");
+    for (j = 0; j < seleccion.Count; j++)
+    {
+        Console.WriteLine(seleccion[j]);
+    }
+}
---
+++ nomina/nomina/Program.cs
@@ -0,0 +1,73 @@
+﻿double valorHora = 0, respuesta = 0, totalHoras = 0, horas = 0, dominicales = 0, totalHorasDominicales = 0, horaDominical = 0, totalPago = 0, total = 0, salario = 1500000;
+string nombre, si;
+bool valido, validoDominicales;
+
+valorHora = salario / 160;
+horaDominical = valorHora * 2;
+
+Console.WriteLine("Ingresa el nombre: ");
+nombre = Console.ReadLine();
+
+Console.WriteLine("Ingrese el total de horas trabajadas: ");
+string entrada = Console.ReadLine();
+valido = double.TryParse(entrada, out horas);
+
+if (!valido || horas < 0)
+{
+    Console.WriteLine("Por favor ingrese un número válido y positivo.");
+}
+else
+{
+    Console.WriteLine("¿Trabajó días dominicales? (si/no): ");
+    si = Console.ReadLine();
+
+    if (si.Length > 2)
+    {
+        Console.WriteLine("El dato ingresado no es válido.");
+    }
+    else
+    {
+        totalHoras = valorHora * horas;
+
+        if (si.ToLower() == "si")
+        {
+            Console.WriteLine("Ingrese cuántas horas dominicales trabajó: ");
+            string entradaDominicales = Console.ReadLine();
+            validoDominicales = double.TryParse(entradaDominicales, out dominicales);
+
+            if (!validoDominicales || dominicales < 0)
+            {
+                Console.WriteLine("Ingrese un dato válido.");
+            }
+            else
+            {
+                totalPago = (valorHora * horas) + (dominicales * horaDominical);
+                totalHorasDominicales = horaDominical * dominicales;
+
+                if (totalPago < salario)
+                {
+                    totalPago = salario;
+                }
+
+                Console.WriteLine($"\nResumen de la nómina de {nombre}");
+                Console.WriteLine($"Horas normales: {horas} x {valorHora} = {totalHoras}");
+                Console.WriteLine($"Horas dominicales: {dominicales} x {horaDominical} = {totalHorasDominicales}");
+                Console.WriteLine($"Total a pagar: {totalPago}");
+            }
+        }
+        else if (si.ToLower() == "no")
+        {
+            respuesta = 0;
+            totalPago = totalHoras;
+
+            Console.WriteLine($"\nResumen de la nómina de {nombre}");
+            Console.WriteLine($"Horas normales: {horas} x {valorHora} = {totalHoras}");
+            Console.WriteLine($"Horas dominicales: {respuesta} x {horaDominical} = {respuesta}");
+            Console.WriteLine($"Total a pagar: {totalPago}");
+        }
+        else
+        {
+            Console.WriteLine("Respuesta inválida. Solo se permite 'si' o 'no'.");
+        }
+    }
+}
